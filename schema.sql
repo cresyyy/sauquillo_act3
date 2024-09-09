74fd7237147a27c287fd9651e67e28350d4310a4
cresyyy <94506717+cresyyy@users.noreply.github.com>
2024-09-09 14:43:44 +0800

    first commit

---
+++ schema.sql
@@ -0,0 +1,37 @@
+CREATE TABLE Friends (
+    FriendID INT AUTO_INCREMENT PRIMARY KEY,
+    FriendWhoAdded INT,
+    FriendBeingAdded INT,
+    IsAccepted BOOLEAN,
+    DateAdded TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+-- Create Groups Table
+CREATE TABLE Groups (
+    GroupID INT AUTO_INCREMENT PRIMARY KEY,
+    GroupName VARCHAR(100),
+    CreatedBy INT,
+    DateAdded TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+-- Create Posts Table
+CREATE TABLE Posts (
+    PostID INT AUTO_INCREMENT PRIMARY KEY,
+    PostDescription VARCHAR(255),
+    PostedBy INT,
+    IsPublic BOOLEAN,
+    IsOnlyForFriends BOOLEAN,
+    GroupID INT,
+    DatePosted TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Create Group Membership Requests Table
+CREATE TABLE GroupMembershipRequests (
+    GroupMemberShipRequestsID INT AUTO_INCREMENT PRIMARY KEY,
+    GroupID INT,
+    GroupMemberUserID INT,
+    IsGroupMemberShipAccepted BOOLEAN,
+    DateAccepted TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
